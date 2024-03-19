13dc2b18f4f6b706a9ce442e297d264dc77d75c8
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2024-03-19 15:02:38 +0100

    beige replaces offwhite on serviceparagraphtextstyles

--- web/src/styles/typography/ParagraphTextStyles.js
+++ web/src/styles/typography/ParagraphTextStyles.js
@@ -12,7 +12,7 @@ export const ParagraphTextStyles = styled.p`
 
 export const ServiceParagraphTextStyles = styled.p`
   font-family: 'Inter', sans-serif;
-  color: #fcfcfa; //offwhite
+  color: #e6e6d3; //beige
   font-size: 1.6rem;
   line-height: 1.5;
   @media only screen and (max-width: 768px) {
