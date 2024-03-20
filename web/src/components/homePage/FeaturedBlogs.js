9d5b662bbcbe1a84c7820080ff6fc0009ca9eb0b
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-03-20 12:23:06 +0200

    new right side experiment, previous one is not behaving as expected

--- web/src/components/homePage/FeaturedBlogs.js
+++ web/src/components/homePage/FeaturedBlogs.js
@@ -44,7 +44,7 @@ function FeaturedBlogs() {
         all your transfer needs with efficiency and reliability.
       </ParagraphText>
       <SectionTitle>Dedication & Reliability</SectionTitle>
-      <ParagraphText className="right">
+      <ParagraphText className="right__side">
         🌐 Connect with Confidence at Stepping Stones Group Internet Cafes! 🌐
         At Stepping Stones Group, our commitment to your connectivity is
         unwavering. 💪 Experience the pinnacle of reliability and dedication as
